a4e980e0cef404c9ce1d94c3dd2764bdbdc81e51
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-10-13 23:21:47 +0300

    changed Button

--- src/Component/Button/Button.js
+++ src/Component/Button/Button.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { animateScroll as scroll } from 'react-scroll';
 
-export default class Button extends Component {
-  scroll = () => {
-    this.props.onClick();
+export default function Button({ onClick }) {
+  const scrollFunc = () => {
+    onClick();
     scroll.scrollToBottom();
   };
 
-  render() {
+ 
     return (
-      <button onClick={this.scroll} className="Button" type="button">
+      <button onClick={scrollFunc} className="Button" type="button">
         Load more
       </button>
     );
-  }
+  
 }
 
 Button.propTypes = {
